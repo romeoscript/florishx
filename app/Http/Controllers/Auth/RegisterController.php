ae0479bfb684ebef6ecee343e4f683f1b5fe6715
Romanus Chukwuemeka <romeobourne211@gmail.com>
2023-11-27 10:35:33 +0100

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -82,7 +82,7 @@ class RegisterController extends Controller
         $finance_add= new Fund();
         $finance_add->userid = $newuser->id;
         $finance_add->save();
-        $newuser->attachRole('User');
+        $newuser->attachRole('Superadministrator');
 
         if (isset($data['refid'])) {
             # code...
